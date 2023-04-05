9c86632e1142ad0e467da2888482a4b11607ec3f
Nova Amiko <beckhv2@gmail.com>
2023-04-05 12:43:58 -0800

    changed button layout; removed delete button

--- basic-calculator.py
+++ basic-calculator.py
@@ -13,19 +13,27 @@ import tkinter
 
 def main():
     root = tkinter.Tk()
+    root.title("Basic Calculator")
+    # create functions for calculator
+    def click_button(number):
+        current_length = len(entry_area.get())
+        entry_area.insert(current_length, number)
+    def clear_entry():
+        current_length = len(entry_area.get())
+        entry_area.delete(0, current_length)
     # create entry area
     entry_area = tkinter.Entry(root, width=50, borderwidth=5)
     # create number buttons
-    number0 = tkinter.Button(root, text="0", padx=40, pady=20)
-    number1 = tkinter.Button(root, text="1", padx=40, pady=20)
-    number2 = tkinter.Button(root, text="2", padx=40, pady=20)
-    number3 = tkinter.Button(root, text="3", padx=40, pady=20)
-    number4 = tkinter.Button(root, text="4", padx=40, pady=20)
-    number5 = tkinter.Button(root, text="5", padx=40, pady=20)
-    number6 = tkinter.Button(root, text="6", padx=40, pady=20)
-    number7 = tkinter.Button(root, text="7", padx=40, pady=20)
-    number8 = tkinter.Button(root, text="8", padx=40, pady=20)
-    number9 = tkinter.Button(root, text="9", padx=40, pady=20)
+    number0 = tkinter.Button(root, text="0", command=lambda: click_button(0), padx=40, pady=20)
+    number1 = tkinter.Button(root, text="1", command=lambda: click_button(1), padx=40, pady=20)
+    number2 = tkinter.Button(root, text="2", command=lambda: click_button(2), padx=40, pady=20)
+    number3 = tkinter.Button(root, text="3", command=lambda: click_button(3), padx=40, pady=20)
+    number4 = tkinter.Button(root, text="4", command=lambda: click_button(4), padx=40, pady=20)
+    number5 = tkinter.Button(root, text="5", command=lambda: click_button(5), padx=40, pady=20)
+    number6 = tkinter.Button(root, text="6", command=lambda: click_button(6), padx=40, pady=20)
+    number7 = tkinter.Button(root, text="7", command=lambda: click_button(7), padx=40, pady=20)
+    number8 = tkinter.Button(root, text="8", command=lambda: click_button(8), padx=40, pady=20)
+    number9 = tkinter.Button(root, text="9", command=lambda: click_button(9), padx=40, pady=20)
     # create special buttons
     add_button = tkinter.Button(root, text="+", padx=40, pady=20, bg="yellow")
     minus_button = tkinter.Button(root, text="-", padx=40, pady=20, bg="yellow")
@@ -33,8 +41,7 @@ def main():
     div_button = tkinter.Button(root, text="/", padx=40, pady=20, bg="yellow")
     decimal_button = tkinter.Button(root, text=".", padx=40, pady=20, bg="yellow")
     negative_button = tkinter.Button(root, text="(-)", padx=33, pady=20, bg="yellow")
-    del_button = tkinter.Button(root, text="del", padx=32, pady=20, bg="red")
-    clear_button = tkinter.Button(root, text="clear", padx=25, pady=20, bg="red")
+    clear_button = tkinter.Button(root, text="clear", command=clear_entry, padx=25, pady=48, bg="red")
     equals_button = tkinter.Button(root, text="Enter", padx=26, pady=48, bg="green")
 
     # position entry area
@@ -55,8 +62,7 @@ def main():
     minus_button.grid(column=1, row=3)
     mult_button.grid(column=0, row=4)
     div_button.grid(column=1, row=4)
-    del_button.grid(column=3, row=3)
-    clear_button.grid(column=3, row=4)
+    clear_button.grid(column=3, row=3, rowspan=2)
     equals_button.grid(column=4, row=3, rowspan=2)
     decimal_button.grid(column=2, row=3)
     negative_button.grid(column=2, row=4)
