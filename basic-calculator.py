97a90c4fe21f95361efc4466ef22940b4fcc2eb6
Nova Amiko <beckhv2@gmail.com>
2023-04-05 09:04:59 -0800

    started file with documentation and import statement

---
+++ basic-calculator.py
@@ -0,0 +1,11 @@
+"""
+Title: Basic Calculator
+Description: Uses tkinter module to make calculator GUI
+Last Updated: April 5, 2023
+Developer: Alexander Beck
+Email: beckhv2@gmail.com
+Github: https://github.com/bexcoding
+"""
+
+
+import tkinter
