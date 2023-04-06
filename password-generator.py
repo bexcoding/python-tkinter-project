a25f45c15860fc97e7431974fb1a813b11d15a0f
Nova Amiko <beckhv2@gmail.com>
2023-04-05 19:37:13 -0800

    added empty file to repository

---
+++ password-generator.py
@@ -0,0 +1,13 @@
+"""
+Title: Password Generator
+Description: GUI version of a password generator
+Last Updated: April 5, 2023
+Developer: Alexander Beck
+Email: beckhv2@gmail.com
+Github: https://github.com/bexcoding
+"""
+
+
+import tkinter
+
+
